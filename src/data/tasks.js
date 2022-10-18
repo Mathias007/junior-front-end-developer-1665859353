7c9b78be2555892fdc9b01de7d9a93705da14126
Mathias007 <mathias.najlepszy@gmail.com>
2022-10-18 19:39:08 +0200

    update tasks.js

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -43,7 +43,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 1: Application has been accepted",
+                title: "Context 1: Application has been accepted",
                 content:
                     "Task 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -52,7 +52,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 1: Application has been accepted",
+                title: "Context 2: Application has been accepted",
                 content:
                     "Task 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -61,7 +61,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 1: Application has been accepted",
+                title: "Context 3: Application has been accepted",
                 content:
                     "Task 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -77,7 +77,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 2: Application has been accepted",
+                title: "Context 1: Application has been accepted",
                 content:
                     "Task 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -86,7 +86,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 2: Application has been accepted",
+                title: "Context 2: Application has been accepted",
                 content:
                     "Task 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -95,7 +95,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 2: Application has been accepted",
+                title: "Context 3: Application has been accepted",
                 content:
                     "Task 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -111,7 +111,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 3: Application has been accepted",
+                title: "Context 1: Application has been accepted",
                 content:
                     "Task 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -120,7 +120,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 3: Application has been accepted",
+                title: "Context 2: Application has been accepted",
                 content:
                     "Task 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -129,7 +129,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 3: Application has been accepted",
+                title: "Context 3: Application has been accepted",
                 content:
                     "Task 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -145,7 +145,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 4: Application has been accepted",
+                title: "Context 1: Application has been accepted",
                 content:
                     "Task 4: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -154,7 +154,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 4: Application has been accepted",
+                title: "Context 2: Application has been accepted",
                 content:
                     "Task 4: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
@@ -163,7 +163,7 @@ export const tasksList = [
                 created_at: "2015-03-25",
                 author: "Olga Nelson",
                 read: true,
-                title: "Task 4: Application has been accepted",
+                title: "Context 3: Application has been accepted",
                 content:
                     "Task 4: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id porttitor nisl, sed bibendum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a fermentum turpis. Integer molestie eleifend tellus, eu vestibulum felis consectetur nec. Maecenas vestibulum felis lorem, eget congue lectus cursus quis. Curabitur eu libero commodo, elementum turpis sit amet, tincidunt urna. Nam accumsan ligula quis eros tempor, eu pharetra massa volutpat. Quisque tincidunt consectetur erat sit amet congue. Nam metus ex, fermentum sit amet tortor at, laoreet hendrerit sem. Sed gravida cursus purus ac varius. Nullam et purus suscipit, interdum nibh a, finibus dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed augue vulputate, tristique velit nec, efficitur nisl. Nullam a posuere eros. Donec aliquet diam a scelerisque tempor. Pellentesque quis eleifend mauris, ac fermentum enim.",
             },
